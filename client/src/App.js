c431c3872cc555789f8ac826e69995223738c5e6
VasiliyMac <cartario@yandex.ru>
2020-11-01 13:11:22 +0300

    post

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState("");  
+  const [data, setData] = useState(""); 
+  const [data2, setData2] = useState(""); 
+  const [form, setForm] = useState(""); 
+console.log(data2)
+  const handleChange = (e) => {
+    setForm(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    axios.post("/hello", {message: form}).then((res)=>setData2(res.data.message));
+  }
 
   useEffect(()=> {
     fetch("/hello").then((res)=>res.json()).then((json)=>setData(json));
@@ -10,7 +21,16 @@ function App() {
 
   return (
     <div className="App">
+      <p>get request:</p>
       {!!data.message ? data.message : ""}
+      <br/>
+      <input 
+        value={form}
+        onChange ={handleChange}
+      />
+      <button onClick = {handleSubmit}>Send</button>
+      <p>post request:</p>
+      {!!data2 ? data2 : ""}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+var cors = require('cors')
 
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.get('/hello', (req, res)=>{
+app.get('/', (req, res)=>{  
+  res.status(200);
+});
+
+app.get('/hello', (req, res)=>{  
   res.send({message: 'hello'});  
 });
+
+app.post('/hello', (req, res)=>{  
+  const message = req.body.message;
+  res.send({message});
+})
 
 app.listen(PORT || 5000, ()=> {
   console.log(`server running on port ${PORT}`)
